e6446a4951d9d650d5132e97d4a8fbb679688e39
Tierra <sr.undershaft@gmail.com>
2019-06-28 11:00:01 -0500

    rearranged javascript to create a nextlevel function and updatd some styling on the game-graphic div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,64 +11,63 @@ $("#help-btn").click(function(){
 // array to hold guesses, keyboard listener, function to check if key is in word
 // functions to do something pretty when you guess wrong
 
-let wordBank = ["home","main", "apartment","fish market","Holcombe Boulevard"];
-let lettersGuessed = [];
+let wordBank = ["home","main", "apartment","fish market","holcombe boulevard"];
 
-let guessesRemaining = 10;
+
+let guessesRemaining = 6;
 let livesRemaining = 9;
-let level = 0;
-
-
-
-document.getElementById("help-btn").onclick = function () {
-    let word = wordBank[level];
-    let blanks = [];
-
-    for (var counter = 0; counter < word.length; counter ++) {
-        blanks = blanks.concat("__ ");
-    }
-
-    document.getElementById("word").innerHTML = blanks.join(" ");
-    document.getElementById("letters-guessed").innerHTML = "You have already guessed: " + lettersGuessed;
-    document.getElementById("guesses-remaining").innerHTML = "You have " + guessesRemaining + " lives remaining"
-    document.onkeyup = function(event) {
-        var userGuess = event.key;
-                
-        if (lettersGuessed.includes(userGuess)) {
-            
-        }
-        else if (word.includes(userGuess)) {
-            var pos = word.indexOf(userGuess);
-            blanks[pos] = userGuess;
-            document.getElementById("word").innerHTML = blanks.join(" ");
-            lettersGuessed = lettersGuessed.concat([userGuess]);
-        }
-        else {
-            
-            guessesRemaining = guessesRemaining -1;
-            document.getElementById("guesses-remaining").innerHTML = "You have " + guessesRemaining + " lives remaining";
-            lettersGuessed = lettersGuessed.concat([userGuess]);
-        }
-        
-        document.getElementById("letters-guessed").innerHTML = "You have already guessed: " + lettersGuessed;
-
-        if (blanks.join("") === word) {
-            document.getElementById("game-graphic").innerHTML = "Congratulations! You have beat level one and helped Smoosh get closer to home. Continue to level 2";
-        }
-    }
-}
-
-
+let level = -1;
+let word = wordBank[level];
+let blanks = [];
 
 function newLevel () {
     level++;
     word = wordBank[level];
     blanks = [];
     lettersGuessed = []
-    document.getElementById("word").innerHTML = blanks.join(" ");
-    document.getElementById("letters-guessed").innerHTML = "You have already guessed: " +lettersGuessed;
-    document.getElementById("guesses-remaining").innerHTML = "You have " + guessesRemaining + " lives remaining";
     for (var counter = 0; counter < word.length; counter++) {
         blanks = blanks.concat("__ "); 
+    }
+    document.getElementById("word").innerHTML = blanks.join(" ");
+    document.getElementById("letters-guessed").innerHTML = "You have already guessed: " +lettersGuessed;
+    document.getElementById("guesses-remaining").innerHTML = "You have " + guessesRemaining + " guesses remaining";
+    document.getElementById("lives-remaining").innerHTML = "You have " + livesRemaining + " lives remaining";
+
+
+    document.getElementById("next-lvl").classList.add("hidden");
+}
+
+
+document.getElementById("help-btn").onclick = function () {
+    newLevel();    
+}
+
+document.onkeyup = function(event) {
+    if (level ===-1) {
+        return;
+    }
+    var userGuess = event.key;
+            
+    if (lettersGuessed.includes(userGuess)) {
+        
+    }
+    else if (word.includes(userGuess)) {
+        var pos = word.indexOf(userGuess);
+        blanks[pos] = userGuess;
+        document.getElementById("word").innerHTML = blanks.join(" ");
+        lettersGuessed = lettersGuessed.concat([userGuess]);
+    }
+    else {
+        
+        guessesRemaining = guessesRemaining -1;
+        document.getElementById("guesses-remaining").innerHTML = "You have " + guessesRemaining + " lives remaining";
+        lettersGuessed = lettersGuessed.concat([userGuess]);
+    }
+    
+    document.getElementById("letters-guessed").innerHTML = "You have already guessed: " + lettersGuessed;
+
+    if (blanks.join("") === word) {
+        document.getElementById("game-state").innerHTML = "Congratulations! You have beat level one and helped Smoosh get closer to home. Continue to level 2";
+        document.getElementById("next-lvl").classList.remove("hidden");
     }
 }
